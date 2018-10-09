07e6688dc1b5d4527d34a4c7c64625a76aeda575
imranKhan2018 <imran1995bd@gmail.com>
2018-10-09 14:09:30 +0600

    update code

--- app/src/main/java/com/example/root/officeapp/ServiceChargeActivity.java
+++ app/src/main/java/com/example/root/officeapp/ServiceChargeActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
